70a9a8c62e748fe0037b769f15b6e4b28ac51399
Sean R Reid <sean@torchcodelab.com>
2020-10-12 20:58:57 -0400

    Added useState and useEffect hooks to the IsueList.jsx component, making it fully functional

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,60 +1,57 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Link } from "react-router-dom";
 
+// Helpers
 import { loadData } from "../utils/loadData";
 
-import { Title } from "bloomer";
-
+// Components
 import Issue from "./Issue";
 
-class IssueList extends Component {
-  state = {
-    issues: [],
-  };
+// Styles
+import { Title } from "bloomer";
 
-  async componentDidMount() {
-    const issues = await loadData(
-      `https://api.github.com/repos/facebook/create-react-app/issues`
-    );
+const IssueList = (props) => {
+  // Set our initial state
+  const [issues, setIssues] = useState([]);
 
-    this.setState({
-      issues,
-    });
-  }
+  // useEffect Hook works almost like a lifecycle method
+  // We can use this hook, and an IIFE, to go and fetch our data and load the state
+  useEffect(() => {
+    (async function () {
+      const issues = await loadData(
+        `https://api.github.com/repos/facebook/create-react-app/issues`
+      );
+      setIssues(issues);
+    })();
+  }, [setIssues]);
 
-  render() {
-    const { issues } = this.state;
-
-    return (
-      <>
-        {!!issues.length ? (
-          <>
-            <Route exact path="/">
-              <Title isSize={2} tag="h1">
-                Github Issues List
-              </Title>
-              <ul>
-                {issues.map((issue) => (
-                  <li key={issue.id}>
-                    {issue.title}
-                    <br />
-                    <Link to={`/issue/${issue.number}`}>
-                      View Issue Details
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Route>
-            <Route path={`/issue/:issue_number`}>
-              <Issue issues={issues} />
-            </Route>
-          </>
-        ) : (
-          <p>Fetching issues...</p>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Title isSize={2} tag="h1">
+        Github Issues List
+      </Title>
+      {!!issues.length ? (
+        <>
+          <Route exact path="/">
+            <ul>
+              {issues.map((issue) => (
+                <li key={issue.id}>
+                  {issue.title}
+                  <br />
+                  <Link to={`/issue/${issue.number}`}>View Issue Details</Link>
+                </li>
+              ))}
+            </ul>
+          </Route>
+          <Route path={`/issue/:issue_number`}>
+            <Issue issues={issues} />
+          </Route>
+        </>
+      ) : (
+        <p>Fetching issues...</p>
+      )}
+    </>
+  );
+};
 
 export default IssueList;
